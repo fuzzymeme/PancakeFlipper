6f8d8a85e41b4f59049979c67e5575dfabd256c1
Richard Smith <richardsmith@Kitty.local>
2017-04-14 19:40:17 +0100

    Permutation generator to help with benchmarking the Pancake Flipper. It will generate all permutations of piles of pancakes for the Flipper to solve.

---
+++ PermutationGenerator.java
@@ -0,0 +1,62 @@
+package pancakeflipperrevisited;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class PermutationGenerator {
+
+	private int count = 0;
+	private int[] original;
+	private List<List<Integer>> permutations;
+
+	public List<List<Integer>> getAllPermutations(int len) {
+		count = 0;
+		permutations = new ArrayList<List<Integer>>();
+		original = getOrg(len);
+		permute(0, new LinkedList<Integer>(), new boolean[original.length]);
+		return permutations;
+	}
+	
+	private void permute(int level, List<Integer> permedList, boolean used[]) {
+		
+		int length = original.length;
+		
+		if (level == length) {
+			permutations.add(permedList);
+			count++;
+		} else {
+			for (int i = 0; i < length; i++) {
+				if (!used[i]) {
+					used[i] = true;
+					LinkedList<Integer> newList = new LinkedList<Integer>();
+					newList.addAll(permedList);
+					newList.add(original[i]);
+					permute(level + 1, newList, used);
+					used[i] = false;
+				}
+			}
+		}
+	}
+	
+	private int[] getOrg(int len){
+
+		int[] org = new int[len];
+		for(int i = 0; i < len; i++){
+			org[i] = i;
+		}
+		return org;
+	}
+
+	public static void main(String[] args) {
+
+		PermutationGenerator pg = new PermutationGenerator();
+//		String s = "0123";
+		pg.getAllPermutations(5);
+		System.out.println("Count:" + pg.count);
+		
+		System.out.println("Perms: " + pg.permutations);
+	}
+
+
+}
