691b1824e0ffbf444683b8de1af21559a435b4a8
fuzzymeme <fuzzymeme@gmail.com>
2017-04-20 12:42:43 +0100

    A little more information on failures and successes.

--- FlipperBenchmarker.java
+++ FlipperBenchmarker.java
@@ -12,7 +12,7 @@ public class FlipperBenchmarker {
 		PermutationGenerator permGenerator = new PermutationGenerator();
 		List<List<Integer>> permutations = permGenerator.getAllPermutations(length);
 				
-		int totalFlips = 0;
+		int totalFlips = 0, failures = 0, successes = 0;
 		for(List<Integer> permutation: permutations) {
 			SequenceBasedFlipper flipper = new SequenceBasedFlipper();
 			SequencedPancakeStack pStack = new SequencedPancakeStack(permutation);
@@ -24,18 +24,21 @@ public class FlipperBenchmarker {
 			OptionalInt returnOptional = flipper.flipUntilCorrectlyStacked(pStack);
 			if(returnOptional.isPresent()) {
 				totalFlips += returnOptional.getAsInt();
+				successes++;
+			} else {
+				failures++;
 			}
 		}
 		
 		double averageFlips = (double) totalFlips / (double) permutations.size();
 		
-		System.out.println("Total flips: " + totalFlips + ", average: " + averageFlips);
+		System.out.println("Total flips: " + totalFlips + ", average: " + averageFlips + ", failures: " + failures + ", successes: " + successes);
 	}
 	
 	public static void main(String[] args) {
 		
 		FlipperBenchmarker benchmarker = new FlipperBenchmarker();
-		benchmarker.runAllAgainstPilesOfLength(4);
+		benchmarker.runAllAgainstPilesOfLength(5);
 	}
 
 }
