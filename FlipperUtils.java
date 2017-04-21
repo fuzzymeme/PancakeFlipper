452092da983fee0c10657f5db5d116b851162ab5
fuzzymeme <fuzzymeme@gmail.com>
2017-04-21 13:30:11 +0100

    Removed commented out code.

--- FlipperUtils.java
+++ FlipperUtils.java
@@ -1,37 +1,6 @@
 package pancakeflipperrevisited;
 
-import java.awt.Point;
-import java.util.List;
-
 public class FlipperUtils {
-
-
-//	public static boolean isCorrectlyStacked(List<Integer> stack){
-//
-//		int i = 0;
-//		while(i < stack.size()){
-//			if(stack.get(i) != i){
-//				return false;
-//			}
-//			i++;
-//		}	
-//		return true;
-//	}
-
-//	public static Point find(int topPancakeSize, PancakeStack stack){
-//
-//		Point pairsPostions = new Point(-1, -1);
-//		for(int i = 0; i < stack.size(); i++){
-//			if(stack.get(i) == (topPancakeSize -1)){
-//				pairsPostions.x = stack.size() - i;
-//			}
-//			if(stack.get(i) == (topPancakeSize +1)){
-//				pairsPostions.y = stack.size() - i;
-//			}
-//		}
-//
-//		return pairsPostions;
-//	}
 
 	public static String toStringSequence(PancakeSequence seq, PancakeStack stack) {
 		
